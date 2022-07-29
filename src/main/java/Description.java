e24f2300da407aa41ab783f895c8ac54dd7e217b
Overglitch_ <phmrlldz@gmail.com>
2022-07-28 21:30:33 -0500

    Update

---
+++ src/main/java/Description.java
@@ -0,0 +1,22 @@
+public class Description implements DeviceBuilder{
+
+    @Override
+    public void setMotherboard(String motherboard) {
+        System.out.println("Motherboard: " + motherboard);
+    }
+    @Override
+    public void setCpu(String cpu) {
+        System.out.println("CPU: " + cpu);
+    }
+    @Override
+    public void setRam(String ram) {
+        System.out.println("RAM: " + ram);
+    }
+    @Override
+    public void setGpu(String gpu) {
+        System.out.println("GPU: " + gpu);
+    }
+    public void setPsu(String psu) {
+        System.out.println("PSU: " + psu);
+    }
+}
---
+++ src/main/java/DeviceBuilder.java
@@ -0,0 +1,6 @@
+public interface DeviceBuilder {
+    void setMotherboard(String motherboard);
+    void setCpu(String cpu);
+    void setRam(String ram);
+    void setGpu(String gpu);
+}
