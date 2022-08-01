83f65c8f46c64907676af8bac4375aa1df9e9615
MiguelHRLX <miguel.huamani.r@uni.pe>
2022-07-31 21:59:00 -0500

    desktop builder

---
+++ src/main/java/DesktopBuilder.java
@@ -0,0 +1,36 @@
+public class DesktopBuilder implements DeviceBuilder {
+    private Desktop desktop;
+
+    @Override
+    public void setMotherboard(String motherboard) {
+        this.desktop.motherboard = motherboard;
+        System.out.println("Motherboard: " + motherboard);
+    }
+
+    @Override
+    public void setCpu(String cpu) {
+        this.desktop.cpu = cpu;
+        System.out.println("CPU: " + cpu);
+    }
+
+    @Override
+    public void setRam(String ram) {
+        this.desktop.ram = ram;
+        System.out.println("RAM: " + ram);
+    }
+
+    @Override
+    public void setGpu(String gpu) {
+        this.desktop.gpu = gpu;
+        System.out.println("GPU: " + gpu);
+    }
+
+    public void setPsu(String psu) {
+        this.desktop.psu = psu;
+        System.out.println("PSU: " + psu);
+    }
+
+    public Desktop returnDesktop() {
+        return this.desktop;
+    }
+}
