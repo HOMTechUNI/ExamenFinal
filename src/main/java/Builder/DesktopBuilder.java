8c9d009d562474f52a8cbd55e00abba566002701
MiguelHRLX <miguel.huamani.r@uni.pe>
2022-08-02 13:30:09 -0500

    delete iteretor device

--- src/main/java/Builder/DesktopBuilder.java
+++ src/main/java/Builder/DesktopBuilder.java
@@ -16,36 +16,30 @@ public class DesktopBuilder implements DeviceBuilder {
     @Override
     public void setMotherboard(Motherboard motherboard) {
         this.desktop.motherboard = motherboard;
-        System.out.println("Components.Motherboard: " + motherboard);
     }
 
     @Override
     public void setCpu(CPU cpu) {
         this.desktop.cpu = cpu;
-        System.out.println("Components.CPU: " + cpu);
     }
 
     @Override
     public void setRam(RAM ram) {
         this.desktop.ram = ram;
-        System.out.println("Components.RAM: " + ram);
     }
 
     @Override
     public void setStorage(Storage storage) {
         this.desktop.storage = storage;
-        System.out.println("Components.Storage: " + storage);
     }
 
     @Override
     public void setGpu(GPU gpu) {
         this.desktop.gpu = gpu;
-        System.out.println("Components.GPU: " + gpu);
     }
 
     public void setPsu(PSU psu) {
         this.desktop.psu = psu;
-        System.out.println("Components.PSU: " + psu);
     }
 
     public Desktop returnDesktop() {
