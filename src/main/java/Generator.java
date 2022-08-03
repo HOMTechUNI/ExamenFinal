2c88d2d729a128335f22df4ad1c77fdf734d192a
MiguelHRLX <miguel.huamani.r@uni.pe>
2022-08-02 22:37:53 -0500

    generator update

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -39,20 +39,6 @@ public class Generator {
         cpus.add(new CPU("CPU10", "Intel Core i5-11600KF", "desktop", 237.99));
     }
 
-    public void addGPUs() {
-        //laptop
-        gpus.add(new GPU("GPU1", "NVIDIA GeForce RTX 3080 Ti Laptop GPU", "12GB", "laptop", 457.99));
-        gpus.add(new GPU("GPU2", "NVIDIA GeForce GTX 1080 SLI", "12GB", "laptop", 352.99));
-        gpus.add(new GPU("GPU3", "Apple M1 8-Core", "8GB", "laptop", 2130.00));
-        gpus.add(new GPU("GPU4", "NVIDIA GeForce RTX 2080", "8GB", "laptop", 2003.99));
-        gpus.add(new GPU("GPU5", "AMD Radeon RX 6800S", "6GB", "laptop", 698.99));
-        //desktop
-        gpus.add(new GPU("GPU6", "Radeon RX 6950 XT", "12GB", "desktop", 799.99));
-        gpus.add(new GPU("GPU7", "GeForce RTX 3090 Ti", "24GB", "desktop", 2269.99));
-        gpus.add(new GPU("GPU8", "NVIDIA Titan RTX", "8GB", "desktop", 3398.99));
-        gpus.add(new GPU("GPU9", "GeForce GTX 1650", "8GB", "desktop", 209.99));
-        gpus.add(new GPU("GPU10", "Radeon RX 6900 XT OC", "6GB", "desktop", 1019.99));
-    }
 
     public void addStorages() {
         //laptop
@@ -67,5 +53,35 @@ public class Generator {
         storages.add(new Storage("STRG8", "HyperX Fury", "120GB", "desktop", 98.99));
         storages.add(new Storage("STRG9", "OCZ ARC 100", "1TB", "desktop", 153.00));
         storages.add(new Storage("STRG10", "Samsung 960", "2TB", "desktop", 265.99));
+    }
+
+    public void addPSUs(){
+        //laptop
+        psus.add(new PSU("PSU1", "Corsair CX450", "laptop", 59.99));
+        psus.add(new PSU("PSU2", "Corsair CX550F", "laptop", 62.10));
+        psus.add(new PSU("PSU3", "XPG Core Reactor", "laptop", 120.00));
+        psus.add(new PSU("PSU4", "Cooler Master V750 Gold V2", "laptop", 159.99));
+        psus.add(new PSU("PSU5", "be quiet! Dark Power Pro 12", "laptop", 415.34));
+        //desktop
+        psus.add(new PSU("PSU6", "Corsair SF750", "desktop", 169.99));
+        psus.add(new PSU("PSU7", "SilverStone SX1000 SFX-L", "desktop", 323.80));
+        psus.add(new PSU("PSU8", "EVGA SuperNOVA 1600 T2", "desktop", 399.99));
+        psus.add(new PSU("PSU9", "Corsair AX1600i", "desktop", 359.99));
+        psus.add(new PSU("PSU10", "EVGA SuperNOVA 1000 T2", "desktop", 229.99));
+    }
+
+    public void addRAMs() {
+        //laptop
+        rams.add(new RAM("RAM1", "G.Skill Trident Z5 RGB", "laptop", "64GB", 341.99));
+        rams.add(new RAM("RAM2", "Samsung DDR5-4800", "laptop", "32GB", 170.99));
+        rams.add(new RAM("RAM3", "TeamGroup T-Force Xtreem", "laptop", "32GB", 149.99));
+        rams.add(new RAM("RAM4", "Patriot Viper Steel", "laptop", "64GB", 109.99));
+        rams.add(new RAM("RAM5", "Corsair Vengeance RGB Pro", "laptop", "128GB", 206.21));
+        //desktop
+        rams.add(new RAM("RAM6", "Corsair Vengeance LPX", "desktop", "8GB", 59.99));
+        rams.add(new RAM("RAM7", "Lexar Thor", "desktop", "2x8GB", 56.99));
+        rams.add(new RAM("RAM8", "Kingston FURY Renegade", "desktop", "32GB", 459.99));
+        rams.add(new RAM("RAM9", "Teamgroup T-Force Vulcan Z", "desktop", "64GB", 69.99));
+        rams.add(new RAM("RAM10", "G.Skill Ripjaws Serie V", "desktop", "128GB", 96.99));
     }
 }
